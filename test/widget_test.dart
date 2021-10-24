f4e15264dcce383ceb449225220e3e0efd8da112
nidhin579 <nidhinvarghese579@gmail.com>
2021-10-24 12:13:27 +0530

    Speech control widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,12 @@ import 'package:hack_live_translate/main.dart';
 import 'package:speech_to_text/speech_to_text.dart';
 
 void main() {
-  testWidgets('Speech status widget test', (WidgetTester tester) async {
+  testWidgets('Speech control widget test', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
-      home: SpeechStatusWidget(
-        speech: SpeechToText(),
-      ),
+      home: SpeechControlWidget(true, true, () {}, () {}, () {}),
     ));
-    var widget = find.text('Not listening');
+    var widget = find.byType(TextButton).last;
     expect(widget, findsOneWidget);
+    await tester.tap(widget);
   });
 }
